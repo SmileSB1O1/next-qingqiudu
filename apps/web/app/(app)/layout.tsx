36ae47507ea93276246cd958bd45dc01e07c37b3
SmileSB101 <891532752@qq.com>
2025-05-06 18:04:00 +0800

    feat: add page header and app group

---
+++ apps/web/app/(app)/layout.tsx
@@ -0,0 +1,31 @@
+import { ModeSwitcher } from "@/components/mode-switcher";
+import { NavHeader } from "@/components/nav-header";
+import { ThemeSelector } from "@/components/theme-selector";
+import { Separator } from "@workspace/ui/components/separator";
+import { SidebarInset } from "@workspace/ui/registry/deer/components/sidebar";
+
+export default async function AppLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <SidebarInset>
+      <header className="bg-background sticky inset-x-0 top-0 isolate z-10 flex shrink-0 items-center gap-2 border-b">
+        <div className="flex h-14 w-full items-center gap-2 px-4">
+          {/* <SidebarTrigger className="-ml-1.5" /> */}
+          {/* <Separator
+            orientation="vertical"
+            className="mr-2 data-[orientation=vertical]:h-4"
+          /> */}
+          <NavHeader />
+          <div className="ml-auto flex items-center gap-2">
+            <ThemeSelector />
+            <ModeSwitcher />
+          </div>
+        </div>
+      </header>
+      {children}
+    </SidebarInset>
+  );
+}
--- packages/ui/src/registry/deer/components/sidebar/sidebar-separator.tsx
+++ packages/ui/src/registry/deer/components/sidebar/sidebar-separator.tsx
@@ -1,18 +1,27 @@
-import { Separator } from "@radix-ui/react-separator";
+"use client";
+
+import * as React from "react";
+import * as SeparatorPrimitive from "@radix-ui/react-separator";
 import { cn } from "@workspace/ui/lib/utils.js";
 
-function SidebarSeparator({
+function Separator({
   className,
+  orientation = "horizontal",
+  decorative = true,
   ...props
-}: React.ComponentProps<typeof Separator>) {
+}: React.ComponentProps<typeof SeparatorPrimitive.Root>) {
   return (
-    <Separator
-      data-slot="sidebar-separator"
-      data-sidebar="separator"
-      className={cn("bg-sidebar-border mx-2 w-auto", className)}
+    <SeparatorPrimitive.Root
+      data-slot="separator-root"
+      decorative={decorative}
+      orientation={orientation}
+      className={cn(
+        "bg-border shrink-0 data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full data-[orientation=vertical]:h-full data-[orientation=vertical]:w-px",
+        className
+      )}
       {...props}
     />
   );
 }
 
-export { SidebarSeparator };
+export { Separator };
